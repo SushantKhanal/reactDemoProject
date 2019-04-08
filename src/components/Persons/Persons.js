9152148b0e3a50255e8997219d5b1cac15a78764
Sushant Khanal <--global>
2019-04-08 11:06:05 +0545

    Persons is now a stateful component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person,index)=>(
-    <Person 
-        key={person.id} 
-        name={person.name} 
-        age={person.age} 
-        click={props.switchNameHandler}
-        changeName={(event)=>props.nameChangeHandler(event,index)}
-    />
-));
+class Persons extends Component {
+    render () {
+        return this.props.persons.map((person,index)=>(
+                <Person 
+                    key={person.id} 
+                    name={person.name} 
+                    age={person.age} 
+                    click={this.props.switchNameHandler}
+                    changeName={(event)=>this.props.nameChangeHandler(event,index)}
+                />
+        ))}
+}
 
-export default persons;
+export default Persons;
